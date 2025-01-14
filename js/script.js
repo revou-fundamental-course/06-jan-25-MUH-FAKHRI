8a30cc217e3f43695291480d1b72a7e237501ebf
MUH-FAKHRI <muhammadfakhri10822@gmail.com>
2025-01-15 01:19:08 +0700

    hasil latihan

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-function replaceName(){
-let name=prompt("siapakah nama anda?,");
-Document.getElementById("name").inenerHTML=name
-}
+let name = prompt("siapakah nama anda?", "");
+document.getElementById('name-user').innerHTML = name
 
+let start = 0;
+otomatis();
+
+function otomatis()
+{
+    const sliders = document.querySelectorAll(".slider");
+
+    for (let i = 0; i < sliders.length; i++)
+
+     {
+    sliders[i].style.display = "none";
+    }
+
+    if (start >= sliders.length)
+    {
+        start = 0;
+    }
+    sliders[start].style.display = "block";
+    console.log("gambar ke" +start)
+    start++;
+
+    setTimeout(otomatis, 1000)
+    }
